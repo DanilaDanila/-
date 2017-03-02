0b21ed20d88116ea5bd92d2c0e7b5cdacaea9176
Danila <manakov.danila@mail.ru>
2017-03-02 21:08:15 +0300

    Спирограф с переменным количеством колен.
      Спирограф — детская игрушка, состоит из пластмассовой пластины с
    вырезанными кругами разных диаметров и набора колёс меньшего диаметра с
    отверстиями внутри. Метод использования: пластина прикладывается к
    листу бумаги, внутрь выбранного кругового отверстия помещается одно из
    зубчатых колёс, в одно из отверстий которого вставляется шариковая
    ручка или карандаш. Затем зубчатое колесо приводится в движение лёгким
    нажимом на пишущий элемент, который оставляет на бумаге спиральный след.
    
      Эту систему можно усовершенствовать и добавить больше центров
    вращений. Эта программа, используя SFML, эмалирует работу спирографа.
      Проект может сохранять ваши проекты(по нажатию клавиши пробел), если
    передать file.png (имя может быть любым), как параметр.

--- Спирограф/main.cpp
+++ Спирограф/main.cpp
@@ -1,89 +1,154 @@
-
-//
-// Disclamer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// In order to load the resources like cute_image.png, you have to set up
-// your target scheme:
-//
-// - Select "Edit Scheme…" in the "Product" menu;
-// - Check the box "use custom working directory";
-// - Fill the text field with the folder path containing your resources;
-//        (e.g. your project folder)
-// - Click OK.
-//
-
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <math.h>
+#include <iostream>
+using namespace sf;
+
+const double pi=3.1415;
+const double degtorad=180*pi;
+
+class hand
+{
+public:
+    int leng;
+    float rotation;
+    float speed;
+    FloatRect coords;
+    RectangleShape hand;
+    
+    void setup(float x,float y)
+    {
+        coords.left=x;
+        coords.top=y;
+        rotation=0;
+        hand.setSize(Vector2f(5,leng));
+        hand.setFillColor(Color::Green);
+        hand.setOrigin(3,leng);
+        hand.setPosition(coords.left,coords.top);
+        rotation=0;
+    }
+    
+    FloatRect get()
+    {
+        FloatRect c;
+        /*if(rotation==0){
+         c.left=c.left;
+         c.top=c.top+leng;
+         }
+         else
+         {
+         }*/
+        c.left=coords.left+leng*cos(rotation-pi/2);
+        c.top=coords.top+leng*sin(rotation-pi/2);
+        return c;
+    }
+    
+    void update(FloatRect c,float time)
+    {
+        if(time!=0)
+        {rotation+=speed*time;
+            coords=c;
+            hand.setRotation(rotation*180/pi);
+            hand.setPosition(coords.left,coords.top);
+        }
+    }
+    
+    void draw(RenderWindow *window)
+    {
+        window->draw(hand);
+    }
+};
 
 int main(int argc, char const** argv)
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile("icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile("sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile("nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
+    int speed=1;
+    bool show=true;
+    Image fon;
+    fon.create(800,800,Color::White);
+    Texture tfon;
+    tfon.loadFromImage(fon);
+    Sprite sfon;
+    sfon.setTexture(tfon);
+    
+    int m;
+    
+    while(true)
     {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
+        std::cout<<"\nВведите колличество колен\n";
+        std::cin>>m;
+        std::vector<hand> hands(m);
+        
+        for(int i=0;i<m;i++)
         {
-            // Close window: exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
+            std::cout<<"\nВведите длинну "<<i+1<<"\n";
+            std::cin>>hands[i].leng;
+        }
+        
+        for(int i=0;i<m;i++)
+        {
+            std::cout<<"\nВведите скорость "<<i+1<<"\n";
+            std::cin>>hands[i].speed;
+        }
+        
+        for(int i=0;i<m;i++)
+        {
+            if(i==0) hands[i].setup(400,400);
+            else
+            {
+                FloatRect r;
+                r=hands[i-1].get();
+                hands[i].setup(r.left,r.top);
             }
         }
-
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
+        
+        float time;
+        Clock clock;
+        
+        RenderWindow window(VideoMode(800,800),"Spirograph");
+        while (window.isOpen())
+        {
+            time=clock.getElapsedTime().asSeconds()/10;
+            clock.restart();
+            
+            Event event;
+            while (window.pollEvent(event))
+            {
+                if(event.type==event.Closed) window.close();
+            }
+            window.clear(Color::White);
+            
+            if(Keyboard::isKeyPressed(Keyboard::Space)) {fon.saveToFile(argv[1]);while(Keyboard::isKeyPressed(Keyboard::Space));}
+            //if(Keyboard::isKeyPressed(Keyboard::W)) {show=!show;time=0;while(Keyboard::isKeyPressed(Keyboard::W));}
+            
+            for(int i=0;i<m;i++)
+            {
+                if(i==0) hands[i].update(hands[i].coords,time);
+                else
+                {
+                    FloatRect r;
+                    r=hands[i-1].get();
+                    hands[i].update(r,time);
+                }
+            }
+            
+            FloatRect r;
+            r=hands[m-1].get();
+            fon.setPixel(r.left,r.top,Color::Blue);
+            fon.setPixel(r.left-1,r.top,Color::Blue);
+            fon.setPixel(r.left+1,r.top,Color::Blue);
+            fon.setPixel(r.left,r.top-1,Color::Blue);
+            fon.setPixel(r.left,r.top+1,Color::Blue);
+            tfon.loadFromImage(fon);
+            sfon.setTexture(tfon);
+            window.draw(sfon);
+            
+            if(show) for(int i=0;i<m;i++) hands[i].draw(&window);
+            
+            window.display();
+        }
+        for(int i=0;i<800;i++)
+            for(int j=0;j<800;j++)
+                fon.setPixel(i,j,Color::White);
     }
-
-    return EXIT_SUCCESS;
+    return 0;
 }
